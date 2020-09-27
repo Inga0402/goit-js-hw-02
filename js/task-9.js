004c915489e5bf54e4ddb5bbc2f2384093e47d9c
Inga Grigorieva <ingagrigorieva0402@gmail.com>
2020-09-27 19:54:45 +0300

    Сделана ДЗ 2 JS

---
+++ js/task-9.js
@@ -0,0 +1,48 @@
+function isLoginValid(login, min = 4, max = 16) {
+  // Write code under this line
+
+  const loginValid = login.length >= 4 && login.length <= 16;
+  return loginValid;
+}
+
+
+function isLoginUnique(allLogins, login) {
+  "use strict";
+  // Write code under this line
+  if (allLogins.includes(login)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+
+function addLogin(allLogins, login) {
+  "use strict";
+  const SUCCESS = "Логин успешно добавлен!";
+  const REFUSAL = "Такой логин уже используется!";
+  const ERROR = "Ошибка! Логин должен быть размером от 4 до 16 символов";
+  let message;
+  if (isLoginValid(login) !== true) {
+    return (message = ERROR);
+  } else if (isLoginUnique(allLogins, login) === true) {
+    allLogins.push(login);
+    return (message = SUCCESS);
+  } else {
+    return (message = REFUSAL);
+  }
+}
+
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+console.log(addLogin(logins, "Ajax"));
+// 'Логин успешно добавлен!'
+
+console.log(addLogin(logins, "robotGoogles"));
+// 'Такой логин уже используется!'
+
+console.log(addLogin(logins, "Zod"));
+// 'Ошибка! Логин должен быть от 4 до 16 символов'
+
+console.log(addLogin(logins, "jqueryisextremelyfast"));
+// 'Ошибка! Логин должен быть от 4 до 16 символов'
